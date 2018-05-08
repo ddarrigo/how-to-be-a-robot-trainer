bc2dbd6062c1400bc4a870bc2d28dff934ef9253
PaLEoS <info@paleos.it>
2018-05-08 11:39:26 +0200

    Add files via upload

---
+++ A_IR_filtro.ino
@@ -0,0 +1,32 @@
+//non è necessario usare la media mobile in quanto il valore è binarizzato
+
+void IRsinistroFiltro(byte indice){
+  totaleIRsinistro+=digitalRead(11);
+  if(indice>=4-1){
+    if(totaleIRsinistro==0)
+      ostacoloSX=true;
+    else{
+      ostacoloSX=false;
+      totaleIRsinistro=0; //reset
+    }
+  }
+  else{
+    ostacoloSX=false;
+  }
+}
+
+void IRdestroFiltro(byte indice){
+  totaleIRdestro+=digitalRead(12);
+  if(indice>=4-1){
+    if(totaleIRdestro==0)
+      ostacoloDX=true;
+    else{
+      ostacoloDX=false;
+      totaleIRdestro=0;
+    }
+  }
+  else{
+    ostacoloDX=false;  
+  }
+}
+
---
+++ B_rete_neurale.ino
@@ -0,0 +1,46 @@
+bool controlloStato(){
+  bool risposta;
+  byte punteggio=0;
+  int vettoreTemporaneo[5]={0,0,0,0,0};
+  for(byte y=0;y<5;y++){
+    for(byte x=0;x<5;x++){ //in questo caso x si ferma a tre, perché dobbiamo valutare solo i primi tre bit del vettore di stato, quelli dei sensori!
+      if(x<3)
+        vettoreTemporaneo[y]+=stato[x]*rete[x][y];
+      if(x>=3)
+        vettoreTemporaneo[y]+=0;
+    }
+  }
+  for(byte x=0;x<5;x++){  //processo di normalizzazione
+    if(vettoreTemporaneo[x]>=1)
+      vettoreTemporaneo[x]=+1;
+    if(vettoreTemporaneo[x]<=-1)
+      vettoreTemporaneo[x]=-1;
+    Serial.print(vettoreTemporaneo[x]);
+    Serial.print("|");
+  }
+  //Serial.println(" ");
+  for(byte y=0;y<4;y++){
+    punteggio=0;
+    for(byte x=0;x<3;x++){
+      if(stato[x]==memoriaPrestabilita[y][x])
+        punteggio++;
+    }
+    if(punteggio>=3){
+      risposta=true;
+      //Serial.println(punteggio);
+      break;
+    }
+    else
+      risposta=false;        
+  }  
+  //estrapola azione da vettore
+  azione=0;
+  for(byte x=0;x<2;x++){
+    if(vettoreTemporaneo[4-x]>=1)
+      bitSet(azione,x);
+  }
+  //Serial.println(azione);
+  
+  return risposta;  
+}
+

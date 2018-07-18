5d0bbe0913970cef24e5ef3bd42c7d329fd17e4f
PaLEoS <info@paleos.it>
2018-07-18 21:57:15 +0200

    Add files via upload

---
+++ motorCTRL.ino
@@ -0,0 +1,32 @@
+/*void motorCTRL(String dir){ 
+  byte spM=4;  //modulo del vettore speed
+  if(dir=="stop"){
+    myservoL.write(90);
+    myservoR.write(90);
+  }
+  if(dir=="forward"){
+    myservoL.write(90+spM);
+    myservoR.write(90-spM);
+  }
+  if(dir=="backwards"){
+    myservoL.write(90-spM);
+    myservoR.write(90+spM); 
+  }
+  if(dir=="clockwise"){
+    myservoL.write(90+spM);
+    myservoR.write(90+spM); 
+  }
+  if(dir=="counterclockwise"){
+    myservoL.write(90-spM);
+    myservoR.write(90-spM); 
+  }
+}
+*/
+
+void motorCTRLdir(bool Ldir,bool Rdir,bool on_off){
+  byte speed_module=4;
+  
+  
+}
+
+
---
+++ smoothing.ino
@@ -0,0 +1,20 @@
+void IRsmoother(){
+  for(byte x=0;x<5;x++){
+    if(analogRead(x)<512)
+      bitClear(IRs[x],IRindex);
+    else
+      bitSet(IRs[x],IRindex);
+  }
+}
+
+
+
+void buttonSmoother(){
+  if(digitalRead(6)==1)
+    bitSet(buttonState,IRindex);
+  else
+    bitClear(buttonState,IRindex);
+}
+
+
+

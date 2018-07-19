e634a3d1b6c6d58f87c8bc8e954d5c5b98e0ed25
PaLEoS <info@paleos.it>
2018-07-19 14:58:08 +0200

    Add files via upload

---
+++ obstacle_detection.ino
@@ -0,0 +1,20 @@
+void obstacleDetection(bool inhib,bool sup){
+  //byte answer=0;
+  byte score=0;
+  if(!inhib){ //sensors state
+    if(IRs[0]==0||IRs[1]==0)
+      motorState=4;
+    if(IRs[2]==0||IRs[3]==0||IRs[4]==0)
+      motorState=7;
+    for(byte i=0;i<5;i++)
+      if(IRs[i]==0)
+        score++;
+    if(score>=3)
+      motorState=0; //emergency stop
+  }
+  if(!sup){ //following actions
+    motorState=motorState;
+    
+  }
+}
+
